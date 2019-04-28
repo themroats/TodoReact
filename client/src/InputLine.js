70453e226c47700f1d50cd99f6ae0d970098cf44
Otis Smith <themroats@gmail.com>
2019-04-28 19:08:44 -0400

    add todos

--- client/src/InputLine.js
+++ client/src/InputLine.js
@@ -1,10 +1,21 @@
 import * as React from "react";
 
 class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {typedText: ""};
+  }
+  handleTyping(event) {
+    this.setState({typedText: event.target.value})
+  }
+  handleSubmit() {
+    this.props.onSubmit(this.state.typedText);
+    this.setState({typedText: ""});
+  }
   render() {
     return (
       <div>
-        <input type="text"/><button onClick={() => this.props.onSubmit("Test Task")}>Submit</button>
+        <input type="text" value={this.state.typedText} onChange={(e) => this.handleTyping(e)}/><button onClick={() => this.handleSubmit()}>Submit</button>
       </div>
     );
   }
