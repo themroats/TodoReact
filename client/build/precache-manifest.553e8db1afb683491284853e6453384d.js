582defe8784590b89f049adf5376b56fa605cc32
Otis Smith <themroats@gmail.com>
2019-04-29 12:21:00 -0400

    Adding `build` to source control

---
+++ client/build/precache-manifest.553e8db1afb683491284853e6453384d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7532d0e567ae65d950319317bf43b0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9c7be2f6a1ebee61c0",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "d2e942256c1edbd64de8",
+    "url": "/static/js/2.7eb0a008.chunk.js"
+  },
+  {
+    "revision": "ed9c7be2f6a1ebee61c0",
+    "url": "/static/js/main.cda07362.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
